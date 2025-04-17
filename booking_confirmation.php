542e82c0fc39f172f38eea8ef3f137cc1582ef90
Diwas KC <diwaskc12@gmail.com>
2025-04-17 11:40:19 +0545

    made confirmation

---
+++ booking_confirmation.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once "./includes/header.php";
+?>
+
+<h1>Booking confirmed for reference: <?= $_GET['reference'] ?></h1>
--- process_booking.php
+++ process_booking.php
@@ -2,6 +2,10 @@
 
 require_once "./includes/header.php";
 
+if(!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     header("Location: index.php");
@@ -22,3 +26,17 @@ VALUES(?,?,?,?,?,NOW())
 $stmt = mysqli_prepare($conn, $booking_query);
 mysqli_stmt_bind_param($stmt, "iiisi", $user_id, $movie_id, $show_time_id, $booking_reference, $total_price);
 mysqli_stmt_execute($stmt);
+
+$booking_id = mysqli_insert_id($conn);
+
+$booking_seats = explode(",", $selected_seats);
+foreach($booking_seats as $bookingSeat) {
+    $booking_seat_query = "INSERT INTO booking_seats(booking_id,seat_number) VALUES(?,?)";
+    $show_stmt = mysqli_prepare($conn, $booking_seat_query);
+    mysqli_stmt_bind_param($show_stmt, "is", $booking_id, $bookingSeat);
+    mysqli_stmt_execute($show_stmt);
+}
+
+header("Location: booking_confirmation.php?reference=" . $booking_reference);
+exit();
+
